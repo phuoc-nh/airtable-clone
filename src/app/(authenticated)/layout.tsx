e9663b8f5f0c15f2f6e6f87459fc53276e3532ae
Phuoc Nguyen <huuphuoc252513@gmail.com>
2025-02-20 22:20:01 +0700

    navigation components

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -3,6 +3,10 @@ import "~/styles/globals.css";
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 import { SessionProvider } from "next-auth/react";
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "~/components/ui/sidebar";
+import { AppSidebar } from "../components/app-side-bar";
+import TopNavBar from "../components/top-nav-bar";
+import { Separator } from "~/components/ui/separator";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -14,10 +18,35 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-
     <html lang="en" className={`${GeistSans.variable}`}>
       <body>
-        <SessionProvider>{children}</SessionProvider>
+        
+        <SidebarProvider>
+          <AppSidebar />
+            {/* <main>
+              <SidebarTrigger />
+              <SessionProvider>
+                {children}
+              </SessionProvider>
+          </main> */}
+          <SidebarInset>
+            <TopNavBar></TopNavBar>
+            {/* <div className="flex flex-1 flex-col gap-4 p-4">
+              <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                <div className="aspect-video rounded-xl bg-muted/50" />
+                <div className="aspect-video rounded-xl bg-muted/50" />
+                <div className="aspect-video rounded-xl bg-muted/50" />
+              </div>
+              <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+            </div> */}
+            <main>
+                {/* <SidebarTrigger /> */}
+                <SessionProvider>
+                  {children}
+                </SessionProvider>
+            </main>
+          </SidebarInset>
+        </SidebarProvider>
       </body>
     </html>
   );
--- src/app/(authenticated)/page.tsx
+++ src/app/(authenticated)/page.tsx
@@ -1,8 +1,17 @@
 import { getSession } from "next-auth/react";
 import { authConfig } from "~/server/auth/config";
 import { redirect } from "next/navigation";
+import getServerSession from "next-auth";
+import { auth } from "~/server/auth";
+
 export default async function HomePage() {
+  const session = await auth();
+  console.log('session', session);
+
+  if (!session) {
+    return redirect("/signin");
+  }
 
 
-  return <div>asd</div>;
+  return <div>Authenticated Yay</div>;
 }
